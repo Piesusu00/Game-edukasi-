67861441787fec6230df9f04f0856ab5bbca99c9
Piesusu00 <perinaagengo@gmail.com>
2025-05-19 10:12:07 +0700

    Questions.js

---
+++ Questions.js
@@ -0,0 +1,7 @@
+const questions = [
+    { q: "Ibukota Indonesia?", a: "jakarta" },
+    { q: "Planet terbesar di tata surya?", a: "jupiter" },
+    { q: "Simbol kimia air?", a: "h2o" },
+    { q: "Presiden pertama Indonesia?", a: "soekarno" },
+    { q: "Hewan tercepat di darat?", a: "cheetah" },
+];
